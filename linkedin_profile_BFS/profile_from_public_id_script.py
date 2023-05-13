fc135df2ec12942f87541a0161a8a3c65b65fbec
shahjaidev <jaidev.shah@columbia.edu>
2023-05-13 17:21:01 -0400

    scripts

---
+++ linkedin_profile_BFS/profile_from_public_id_script.py
@@ -0,0 +1,36 @@
+#commands to set password
+#export LINKEDIN_PASSWORD="password"
+
+
+#from linkedin_api import Linkedin
+from custom_linkedin_api import Linkedin
+
+import os
+import pickle
+
+password = os.environ.get('LINKEDIN_PASSWORD')
+
+# Authenticate using any Linkedin account credentials
+api = Linkedin('shah.jaidev00@gmail.com', password)
+
+PUBLIC_PROFILE =  "shahjaidev" #"shahjaidev"
+
+
+profile_response_dict = api.get_profile(PUBLIC_PROFILE)
+print(profile_response_dict.keys())
+
+profile_id = profile_response_dict['profile_id']
+print(f"Profile ID: {profile_id}")
+
+
+keys_to_keep = ['industryName', 'lastName', 'firstName', 'geoLocationName', 'headline', 'experience', 'education', 'projects']
+filtered_profile_dict = {key: profile_response_dict[key] for key in profile_response_dict if key in keys_to_keep}
+
+
+
+print(f"Filtered Profile Response: \n {filtered_profile_dict}")
+
+
+
+
+
--- linkedin_profile_BFS/script_README.txt
+++ linkedin_profile_BFS/script_README.txt
@@ -1,5 +1,13 @@
 ##TO Use for quick prompt Engineering
 
+shah.jaidev00@gmail.com
 export LINKEDIN_PASSWORD="hackGPT123"
 
+
+#For getting the filtered profile dict, given a public profile ID
+python profile_from_public_id_script.py
+
+
+
+#To get 2-hop neighbors
 python initial_script_linkedin_api.py
